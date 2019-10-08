4752e5de046c26bbc1eba499df03729180ea480e
Gracherino <55064198+Gracherino@users.noreply.github.com>
2019-10-08 16:44:36 +0300

    Add files via upload

--- links/ForCol1/js/script.js
+++ links/ForCol1/js/script.js
@@ -5,8 +5,9 @@ $(document).ready(function() {
             .addClass('products__tab_active').siblings().removeClass('products__tab_active')
             .closest('div.container').find('div.products__content').removeClass('products__content-active').eq($(this).index()).addClass('products__content-active');
     });
-    el.addClass('anim_tab');
-    [].slice.call(document.querySelectorAll('a[href="#"')).forEach(function(el) {
-        el.addEventListener('click', function(ev) { ev.preventDefault(); });
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > el.offset().top - 200) {
+            el.addClass('anim_tab');
+        }
     });
 });
